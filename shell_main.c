0c76bb8327b0a314223539cbfdfe472247dec778
Okoli Ekene <khennyphresh@gmail.com>
2022-05-19 01:42:18 +0100

    Update shell_main.c

--- shell_main.c
+++ shell_main.c
@@ -1,11 +1,5 @@
 #include "shell.h"
 
-
-	/*char **commands = NULL;
-	char *line = NULL;
-	char *shell_name = NULL;
-	int status = 0;*/
-
 /**
  * main - the main shell code
  * @argc: number of arguments passed
